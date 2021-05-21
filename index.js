699ab0cd2fb568e43db2aaceff6d892129b2de5e
Brianna Gordon <brigordon574@gmail.com>
2021-05-20 19:45:36 -0700

    commit tree 51b2d9ec9bfdb41c3ca4bba6b60e5e0811ccc25e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+//driverName.toUpperCase() === name.toUpperCase();
+
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+ function findMatching(myArray,string){
+ return myArray.filter(driver => driver.toLowerCase() === string.toLowerCase())
+ }
+ findMatching();
+
+function fuzzyMatch(myArray,string){
+    return myArray.filter(driver =>driver.startsWith(string));
+}
+
+function matchName(myArray,string){
+    for (const element in myArray){
+        return myArray.filter(driver => driver.name === string)
+    }
+}
